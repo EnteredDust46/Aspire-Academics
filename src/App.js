f20f1e3006a850592ab8c8ad8a74fa0cce393a3b
Adam Kamenetsky <adam.kamenetsky24@gmail.com>
2025-03-12 02:14:37 -0400

    1.0.11 Bugfix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import './App.css';
